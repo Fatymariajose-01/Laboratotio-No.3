74ae9e99d08c0a12bd22ddef543a944d5d74ac39
Fatymariajose-01 <fmariajose2006@gmail.com>
2024-08-13 21:24:05 -0600

    Agregué la clase Reserva, procederé con la clase plato que creo será la clase padre hasta cierto punto.

---
+++ Laboratotio No.3/Reserva.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratotio_No._3
+{
+    public class Reserva
+    {
+        string NumeroUnico { get; set; }
+        string Fecha { get; set; }
+        string Hora { get; set; }
+
+    }
+}
