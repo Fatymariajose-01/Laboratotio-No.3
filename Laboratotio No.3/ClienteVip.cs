681aa2f984354fab84437068bddfe3329c271d0b
Fatymariajose-01 <fmariajose2006@gmail.com>
2024-08-13 21:18:06 -0600

    Agregué la clase para clientes VIP, (todavía no trabajo en los métodos)

---
+++ Laboratotio No.3/ClienteVip.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratotio_No._3
+{
+    public class ClienteVip:Cliente
+    {
+        double DescuentoEspecial { get; set; }
+
+        public ClienteVip(string nombre, string correoelectronico, string numerodetelefono, double decuentoespecial)
+            :base (nombre, correoelectronico, numerodetelefono )
+        {
+            DescuentoEspecial = decuentoespecial;
+        }
+    }
+}
