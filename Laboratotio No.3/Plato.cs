e391fd932b482537776104f7ba3d7cea55d3873c
Fatymariajose-01 <fmariajose2006@gmail.com>
2024-08-13 22:04:36 -0600

    Terminé y ordené las clases según creo las puedo llegar a utlizar.

--- Laboratotio No.3/Plato.cs
+++ Laboratotio No.3/Plato.cs
@@ -8,13 +8,17 @@ namespace Laboratotio_No._3
 {
     public class Plato:Reserva
     {
+        //Plato tiene la info del cliente y de la reserva.
         public string NombrePlato { get; set; }
         public int PrecioPlato { get; set; }
-        public Plato(string numerounico, string fecha, string hora, int precioLugar, string nombreplato, int precioplato)
-            : base(numerounico, fecha, hora, precioLugar)
-        {
+        public int CantidadPlatos { get; set; }
+        public Plato(string nombre, string correoelectronico, string numerodetelefono, double decuentoespecial, string numerounico, string fecha, string hora, int precioLugar, string nombreplato, int precioplato, int cantidadplatos)
+            : base(nombre, correoelectronico, numerodetelefono, decuentoespecial, numerounico, fecha, hora, precioLugar)
+        { 
             NombrePlato = nombreplato;
             PrecioPlato = precioplato;
-        }
+            CantidadPlatos = cantidadplatos;
+            }
     }
+
 }
--- Laboratotio No.3/Reserva.cs
+++ Laboratotio No.3/Reserva.cs
@@ -6,14 +6,16 @@ using System.Threading.Tasks;
 
 namespace Laboratotio_No._3
 {
-    public class Reserva
+    public class Reserva:ClienteVip
     {
         public string NumeroUnico { get; set; }
         public string Fecha { get; set; }
         public string Hora { get; set; }
         public int PrecioLugar { get; set; }
+        //Si es cliente normal a la hora de pedir descuento agrego un if e igualo el descuento a 0.
 
-        public Reserva(string numerounico, string fecha, string hora, int precioLugar)
+        public Reserva(string nombre, string correoelectronico, string numerodetelefono, double decuentoespecial, string numerounico, string fecha, string hora, int precioLugar)
+             : base(nombre, correoelectronico, numerodetelefono, decuentoespecial)
         {
             NumeroUnico = numerounico;
             Fecha = fecha;
