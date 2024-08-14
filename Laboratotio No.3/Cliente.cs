80e6f7b792cd0ed99f4f28e53554b1dd565f3014
Fatymariajose-01 <fmariajose2006@gmail.com>
2024-08-13 21:09:20 -0600

    Cree la clase cliente junto a su constructor (Pongo en duda si debería usar una interface).

---
+++ Laboratotio No.3/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ObjectiveC;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratotio_No._3
+{
+    public class Cliente
+    {
+        string Nombre { get; set; }
+        string CorreoElectronico { get; set; }
+        string NumeroDeTelefono { get; set; }
+
+        public Cliente(string nombre, string correoelectronico, string numerodetelefono)
+        {
+            Nombre = nombre;
+            CorreoElectronico = correoelectronico;
+            NumeroDeTelefono = numerodetelefono;
+        }
+    }
+}
